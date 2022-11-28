317f2af67e8817f0c56227e0c876958f413946ca
Cristianetaniguti <chaytaniguti@gmail.com>
2022-11-28 11:32:30 -0600

    Empirical tutorial

--- Reads2Map/convert_graphiviz.sh
+++ Reads2Map/convert_graphiviz.sh
@@ -1,9 +1,21 @@
+cd Reads2Map/pipelines
 
-for i in $(cat workflows); do
+for i in $(find . -type f -name "*.wdl"); do
     filename=$(basename -- "$i")
     echo $filename
     filename2=${filename%.*}
     echo $filename2
-    java -jar ~/womtool-49.jar graph $i > images/$filename2.gv
-    dot -Tsvg images/$filename2.gv > images/$filename2.svg
+    java -jar ../../womtool-84.jar graph $i > ../../images/$filename2.gv
+    dot -Tsvg ../../images/$filename2.gv > ../../images/$filename2.svg
+done
+
+cd ../subworkflows
+
+for i in $(find . -type f -name "*.wdl"); do
+    filename=$(basename -- "$i")
+    echo $filename
+    filename2=${filename%.*}
+    echo $filename2
+    java -jar ../../womtool-84.jar graph $i > ../../images/$filename2.gv
+    dot -Tsvg ../../images/$filename2.gv > ../../images/$filename2.svg
 done
